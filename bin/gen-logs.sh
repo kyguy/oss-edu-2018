7f5462d139e53946dc641650aa7fb4a60c9e26bd
Kyle Liberti <kliberti@redhat.com>
2018-10-17 11:27:34 -0400

    adjust gen_logs script

--- bin/gen-logs.sh
+++ bin/gen-logs.sh
@@ -3,7 +3,9 @@ POD=formatted-logs
 PROJECT=instrumented-app
 ARG=($1)
 if [[ $ARG = "loggy" ]]; then
-  oc exec -n $PROJECT -ti $(oc get pods -n $PROJECT | grep $POD | awk '{print $1}') -- curl http://localhost:8080/loggy
+  curl http://logs.ocp311.jkarasek.test/loggy
+  #oc exec -n $PROJECT -ti $(oc get pods -n $PROJECT | grep $POD | awk '{print $1}') -- curl http://localhost:8080/loggy
 else
-  oc exec -n $PROJECT -ti $(oc get pods -n $PROJECT | grep $POD | awk '{print $1}') -- curl http://localhost:8080/generate?logs=$ARG
+  curl http://logs.ocp311.jkarasek.test/generate?logs=$ARG
+  #oc exec -n $PROJECT -ti $(oc get pods -n $PROJECT | grep $POD | awk '{print $1}') -- curl http://localhost:8080/generate?logs=$ARG
 fi
