44f2f0c59a5eb3a92a21365ef24bd2177e8dbd7d
Kyle Liberti <kliberti@redhat.com>
2018-10-17 11:34:43 -0400

    adjusted read_broker script

--- bin/read_brokers.sh
+++ bin/read_brokers.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 TOPIC=($1)
 POD=my-connect-cluster-connect
+
+if [ -z $TOPIC ]; then
+  TOPIC=output_tag
+fi
+
 oc exec -ti $(oc get pods | grep $POD | awk 'NR==1{print $1}') -- ./bin/kafka-console-consumer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic $TOPIC --from-beginning
