39423eb0bc308fb7c414ba65365349e7096df7ac
Kyle Liberti <kliberti@redhat.com>
2018-10-17 13:26:57 -0400

    Fixed aggregator script

--- bin/aggregate_logger.sh
+++ bin/aggregate_logger.sh
@@ -3,6 +3,7 @@ PROJECT=openshift-logging
 POD=logging-fluentd
 COMMAND=($1)
 set -e
+set -x
 
 exec_in_pod () {
   oc exec -ti $( oc get pods -n $PROJECT | grep $POD | awk 'NR==1{print $1}' ) -n $PROJECT -- "$@"
@@ -13,7 +14,7 @@ if [ -z "$COMMAND" ]; then
     ls /var/log/containers
 else
   if [[ $COMMAND = "loggy" ]]; then
-    exec_in_pod tail -f /var/log/containers/formatted-logs*
+    exec_in_pod sh -c "tail -f /var/log/containers/formatted-logs*"
   else	  
     exec_in_pod \
       "$@"
